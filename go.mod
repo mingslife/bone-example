f44ea68d34de94efea32a62fcc3e7bb77b244a42
Ming <642203604@qq.com>
2022-07-08 23:35:23 +0800

    :arrow_up: Upgrade bone

--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/facebookgo/inject v0.0.0-20180706035515-f23751cae28b // indirect
 	github.com/facebookgo/structtag v0.0.0-20150214074306-217e25fb9691 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
-	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/mingslife/bone v0.0.1 // indirect
+	github.com/mingslife/bone v0.0.2 // indirect
 )
--- pkg/module/hello/transport/http.go
+++ pkg/module/hello/transport/http.go
@@ -1,6 +1,9 @@
 package transport
 
 import (
+	"net/http"
+
+	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/mingslife/bone"
 	"github.com/mingslife/bone-example/pkg/module/hello/endpoint"
 )
@@ -12,7 +15,7 @@ type HelloHttp struct {
 }
 
 func (h *HelloHttp) Register() error {
-	h.Router.RegisterEndpoint("GET", "/v1/hello", h.Endpoint.Hello, h.Decoder.Hello)
+	h.Router.Methods(http.MethodGet).Path("/v1/hello").Handler(bone.NewServer(h.Endpoint.Hello, h.Decoder.Hello, httptransport.EncodeJSONResponse))
 	return nil
 }
 
